2c2123636ae0dba0ae431acb99aec556d15b7ace
dekeltsairi <dekeltsairi@gmail.com>
2024-05-05 13:44:00 +0300

    UseHosted service instead of scope

---
+++ API/HostedServices/ScraperHostedService.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Abstractions;
+
+namespace API.HostedServices
+{
+    public class ScraperHostedService : IHostedService
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public ScraperHostedService(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            using var scope = _serviceProvider.CreateScope();
+            var scraper = scope.ServiceProvider.GetRequiredService<IScraper>();
+            await Task.Delay(100000);
+            await scraper.ScrapeAsync();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+    }
+}
